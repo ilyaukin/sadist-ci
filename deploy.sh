1bfdf738654a402d4d97bf08a446f598d4f574fa
Ilya Lyaukin <kzerby@gmail.com>
2020-05-03 19:36:02 +0700

    store secrets w/ \n and evaluate them via echo -e in scripts

--- deploy.sh
+++ deploy.sh
@@ -7,9 +7,9 @@ cp -r /static/*.js /static/img app/static
 
 export DOCKER_CERT_PATH=/cert
 mkdir $DOCKER_CERT_PATH
-echo "$CA" > $DOCKER_CERT_PATH/ca.pem
-echo "$CLIENT_CERT" > $DOCKER_CERT_PATH/cert.pem
-echo "$CLIENT_KEY" > $DOCKER_CERT_PATH/key.pem
+echo -e "$CA" > $DOCKER_CERT_PATH/ca.pem
+echo -e "$CLIENT_CERT" > $DOCKER_CERT_PATH/cert.pem
+echo -e "$CLIENT_KEY" > $DOCKER_CERT_PATH/key.pem
 docker-compose build
 docker-compose down
 docker-compose up -d --force-recreate
