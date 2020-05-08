57d94d0dbfe2fbb723a4b4d71b7422503f26899d
Ilya Lyaukin <kzerby@gmail.com>
2020-05-08 18:10:42 +0700

    moved fe build to the same container

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,15 @@
 
 set -e
 
-mkdir app/static
-cp -r /static/*.js /static/img app/static
-
 export DOCKER_CERT_PATH=/cert
 mkdir $DOCKER_CERT_PATH
 echo -e "$CA" > $DOCKER_CERT_PATH/ca.pem
 echo -e "$CLIENT_CERT" > $DOCKER_CERT_PATH/cert.pem
 echo -e "$CLIENT_KEY" > $DOCKER_CERT_PATH/key.pem
+
 docker-compose build
 docker-compose down
 docker-compose up -d --force-recreate
+
 rm -rf $DOCKER_CERT_PATH
 
