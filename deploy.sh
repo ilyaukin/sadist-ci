c0a2e6acca822ad3e7d0015303c09b1bf631ab6d
Ilya Lyaukin <kzerby@gmail.com>
2022-12-10 22:09:33 -0600

    Revert "passing certs to the inner docker via volume instead of env vars"
    This reverts commit 8bdbd2b6af1bcc4ed02d2acb18b49cdff0c7df62.

--- deploy.sh
+++ deploy.sh
@@ -2,9 +2,18 @@
 
 set -e
 
+export DOCKER_CERT_PATH=/cert
+mkdir $DOCKER_CERT_PATH
+echo -e "$CA" > $DOCKER_CERT_PATH/ca.pem
+echo -e "$CLIENT_CERT" > $DOCKER_CERT_PATH/cert.pem
+echo -e "$CLIENT_KEY" > $DOCKER_CERT_PATH/key.pem
+
 if [ $ENV != "prod" ]; then
   export COMPOSE_PROJECT_NAME=$ENV
 fi
 docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml build
 docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml down
 docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml up -d --force-recreate
+
+rm -rf $DOCKER_CERT_PATH
+
