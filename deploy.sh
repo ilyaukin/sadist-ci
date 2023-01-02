baa5a6082046f916013fab789d426df2d0a25660
Ilya Lyaukin <kzerby@gmail.com>
2023-01-02 10:42:15 -0600

    Pass DATABASE_URL during build time

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ echo -e "$CLIENT_KEY" > $DOCKER_CERT_PATH/key.pem
 if [ $ENV != "prod" ]; then
   export COMPOSE_PROJECT_NAME=$ENV
 fi
-docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml build
+docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml build --build-arg DATABASE_URL="$DATABASE_URL"
 docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml down
 docker-compose -f docker-compose.yml -f docker-compose."$ENV".yml up -d --force-recreate
 
